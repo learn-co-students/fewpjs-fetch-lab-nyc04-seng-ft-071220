95a83e8bb6e594456ef9297af57a47b816db11a8
iannakim <dear.hyunji@gmail.com>
2020-09-16 08:09:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
-  return fetch('https://anapioficeandfire.com/api/books')
+  fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => renderBooks(json));
+  .then(json => {
+    renderBooks(json)
+    
+  });
 }
 
 function renderBooks(books) {
